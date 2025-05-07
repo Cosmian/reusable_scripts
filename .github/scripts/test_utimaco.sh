35be2a9c679630d6990b391018a3bd44c0a71b72
phochard <pauline.hochard@cosmian.com>
2025-05-07 14:51:48 +0200

    move kms test in cargo_build script

--- .github/scripts/test_utimaco.sh
+++ .github/scripts/test_utimaco.sh
@@ -33,8 +33,4 @@ cd ./hsm-simulator/Administration
 ./p11tool2 Slot=0 GetSlotInfo
 cd ../..
 
-if [ "$1" = "Cosmian/kms" ]; then
-    HSM_USER_PASSWORD="12345678" cargo test -p utimaco_pkcs11_loader --target x86_64-unknown-linux-gnu --features utimaco -- tests::test_all
-fi
-
 rm -rf hsm-simulator
