bf1e5b9d57b95cf0a2bc55063e447a9d18f72e82
Manuthor <manu.coste@gmail.com>
2025-06-11 10:11:09 +0200

    test: fix generic loop script

--- loop.sh
+++ loop.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-cargo build --workspace --all-targets
+cargo build --workspace
 
 # export RUST_LOG="cosmian_cli=trace,cosmian_findex_server=trace"
 
@@ -10,6 +10,6 @@ echo "Running tests in an infinite loop"
 while true; do
   reset
   echo "Iteration: $((++count))"
-  cargo test --workspace -- --nocapture remote --include-ignored
+  cargo test --workspace -- --nocapture
   sleep 1
 done
