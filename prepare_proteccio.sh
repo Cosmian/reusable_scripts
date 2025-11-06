f8cfdf193c581a45456e00946db135c613d3ca71
Manuthor <manu.coste@gmail.com>
2025-11-06 11:04:42 +0100

    fix: proteccio tree creation

--- prepare_proteccio.sh
+++ prepare_proteccio.sh
@@ -5,7 +5,7 @@ wget -q https://package.cosmian.com/ci/hsm-proteccio.tar.gz
 tar -xzf hsm-proteccio.tar.gz
 rm hsm-proteccio.tar.gz
 
-mkdir -p /etc/proteccio/
+sudo mkdir -p /etc/proteccio/
 sudo cp proteccio/etc/proteccio/* /etc/proteccio/
 sudo cp proteccio/lib/* /lib/
 sudo cp proteccio/usr/local/bin/* /usr/local/bin/
