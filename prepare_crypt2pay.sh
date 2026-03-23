fc91e9e1ab08f0013145ed0944ea483ba01b8be0
pauline ramon <pauline.hochard@protonmail.com>
2026-03-23 09:44:47 +0100

    prepare c2p script

--- prepare_crypt2pay.sh
+++ prepare_crypt2pay.sh
@@ -18,22 +18,10 @@ cd /etc/c2p/
 # check that the CA certificate is installed correctly
 ./installca -l ./ssl/
 
-
-# Update c2p.xml cert path
-CA_PATH="/etc/c2p/ssl"
-sed -i "s|<Authorities>.*</Authorities>|<Authorities>$CA_PATH</Authorities>|" c2p.xml
-
 # Logging configuration
 LOG_DIR="/etc/c2p/logs"
 sed -i "s|<TraceLevel>.*</TraceLevel>|<TraceLevel>debug functions parameters pkcs hsm</TraceLevel>|" c2p.xml
 sed -i "s|<TraceFile>.*</TraceFile>|<TraceFile>+$LOG_DIR/c2p.trc</TraceFile>|" c2p.xml
 
-export C2P_CONF=/etc/c2p/c2p.xml
-
-# Force fixed HSM IP
-export C2P_HSM_IP="193.251.82.208"
-C2P_XML_FILE="/etc/c2p/c2p.xml"
-sed -i "s#<IP>.*</IP>#<IP>${C2P_HSM_IP}</IP>#" "$C2P_XML_FILE"
-
 # Create a new 256-bit AES key
-./p11tool -genkey -keyalg aes -keysize 256 -shared /lib/libpkcs11c2p.so -slot 1 -verbose
+./p11tool -genkey -keyalg aes -keysize 256 -shared /lib/libpkcs11c2p.so -slot 0 -verbose
