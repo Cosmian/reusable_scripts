08b302f60970c61f283bbb743fedf534e72ace9c
pauline ramon <pauline.hochard@protonmail.com>
2026-04-08 14:17:07 +0200

    fix permissions

--- prepare_crypt2pay.sh
+++ prepare_crypt2pay.sh
@@ -7,21 +7,27 @@ rm hsm-crypt2pay.tar.gz
 
 sudo mkdir -p /etc/c2p/
 sudo cp -R c2p/etc/c2p/* /etc/c2p/
-sudo cp c2p/lib/* /lib/
+sudo cp c2p/lib/* /usr/lib/
 
 rm -rf c2p
 
-# install the CA certificate
-cd /etc/c2p/
-sudo ./installca -i ./ca.der ssl
+# Fix permissions
+sudo chown -R root:root /etc/c2p
+sudo chmod -R 755 /etc/c2p
 
-# check that the CA certificate is installed correctly
+# Create logs dir
+sudo mkdir -p /etc/c2p/logs
+sudo chmod 777 /etc/c2p/logs
+
+cd /etc/c2p/
+
+# install CA
+sudo ./installca -i ./ca.der ssl
 sudo ./installca -l ./ssl/
 
-# Logging configuration
-LOG_DIR="/etc/c2p/logs"
+# Logging config
 sudo sed -i "s|<TraceLevel>.*</TraceLevel>|<TraceLevel>debug functions parameters pkcs hsm</TraceLevel>|" c2p.xml
-sudo sed -i "s|<TraceFile>.*</TraceFile>|<TraceFile>+$LOG_DIR/c2p.trc</TraceFile>|" c2p.xml
+sudo sed -i "s|<TraceFile>.*</TraceFile>|<TraceFile>+/etc/c2p/logs/c2p.trc</TraceFile>|" c2p.xml
 
-# Create a new 256-bit AES key
-sudo ./p11tool -genkey -keyalg aes -keysize 256 -shared /lib/libpkcs11c2p.so -slot 0 -verbose
+# Test HSM
+sudo ./p11tool -genkey -keyalg aes -keysize 256 -shared /usr/lib/libpkcs11c2p.so -slot 0 -verbose
